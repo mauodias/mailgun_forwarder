8bc7a61d9dfa38bdd3415a7e7203c6cc7d1b144d
Maurício Dias <mau.dias@gmail.com>
2020-07-25 03:32:38 +0200

    Changes the format for cloud functions

--- app.py
+++ app.py
@@ -1,34 +1,9 @@
 from flask import Flask, request
+from function import parse
 import os
 import requests
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def parse():
-    base_url = 'https://api.mailgun.net/v3'
-    token = os.environ.get('MAILGUN_TOKEN', None)
-    dest_inbox = os.environ.get('DESTINATION_INBOX', None)
-    if not (token and dest_inbox):
-        return None
-
-    msg = request.form
-    sender = msg['sender']
-    recipient = msg['recipient']
-    mailbox = recipient.split('@')[0]
-    subject = msg['subject']
-    text = msg['stripped-text']
-    html = msg['stripped-html']
-
-    auth = ('api', token)
-    data = {
-        'from': sender,
-        'to': dest_inbox,
-        'subject': f'[{mailbox.upper()}] {subject}',
-        'text': text,
-        'html': html
-    }
-
-    response = requests.post(f'{base_url}/mauricio.cc/messages', data, auth=auth)
-
-    print(response.text, response.status_code)
-    return f'{response.text}', response.status_code
+def caller():
+    return parse(request)
